7b6b1789e179d6e4c8dffbdeca09d7f9ce8d64a8
Mikhail <epatko-m-i@rambler.ru>
2017-01-25 00:51:01 +1000

    Add InteractCalc.

---
+++ chapter_004/interactCalc/src/main/java/ru/epatko/calculator/Calculator.java
@@ -0,0 +1,58 @@
+package ru.epatko.calculator;
+
+/**
+ * @author Mikhail Epatko (epatko-m-i@rambler.ru).
+ *         24.01.17.
+ */
+
+public class Calculator {
+
+	/**
+	 * Operation result.
+	 */
+	private double result;
+
+	/**
+	 * Addition operation.
+	 * @param a - first value.
+	 * @param b - second value.
+	 */
+	public void add(double a, double b) {
+		this.result = a + b;
+	}
+			
+	/**
+	 * Subtraction operation.
+	 * @param a - first value.
+	 * @param b - second value.
+	 */
+	public void sub(double a, double b) {
+		this.result = a - b;
+	}
+		
+	/**
+	 * Multiplication operation.
+	 * @param a - first value.
+	 * @param b - second value.
+	 */
+	public void mult(double a, double b) {
+		this.result = a * b;
+	}
+	
+	/**
+	 * Division operation.
+	 * @param a - first value.
+	 * @param b - second value.
+	 */
+	public void div(double a, double b) {
+		this.result = a / b;
+	}
+
+	/**
+	 * Getter.
+	 * @return - operation result.
+	 */
+	public double getResult() {
+		return this.result;
+	}
+}
---
+++ chapter_004/interactCalc/src/main/java/ru/epatko/calculator/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Mikhail Epatko (epatko-m-i@rambler.ru).
+ * 24.01.17.
+ */
+package ru.epatko.calculator;
