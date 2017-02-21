34c561fb0dd12222dad24df00fe02c04d7b8f451
Mikhail <epatko-m-i@rambler.ru>
2017-02-21 10:07:34 +1000

    Added Leaf.class into MyTree.class.

--- chapter_005/tree/src/main/java/ru/epatko/myTree/MyTree.java
+++ chapter_005/tree/src/main/java/ru/epatko/myTree/MyTree.java
@@ -3,6 +3,7 @@ package ru.epatko.myTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Mikhail Epatko (mikhail.epatko@gmail.com).
@@ -12,6 +13,69 @@ public class MyTree<E> {
 
     private Leaf<E> root;
 
+
+
+    /**
+     * Basic leaf.
+     */
+    static class Leaf<E> implements Comparable<Leaf<E>> {
+
+        final int hash;
+        final E value;
+        Leaf<E> left;
+        Leaf<E> right;
+        Leaf<E> parent;
+
+        /**
+         * Constructs leaf.
+         * @param value object {@code E} type
+         * @param left left leaf
+         * @param right right leaf
+         * @param parent parent leaf
+         */
+        public Leaf(final E value, Leaf<E> parent, Leaf<E> left, Leaf<E> right) {
+            this.value = value;
+            hash = Objects.hashCode(value);
+            this.left = left;
+            this.right = right;
+            this.parent = parent;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Value: %s", value.toString());
+        }
+
+        @Override
+        public final boolean equals(Object o) {
+            boolean result = false;
+            if (o == this) {
+                result = true;
+            } else if (o instanceof Leaf) {
+                Leaf<E> e = (Leaf<E>) o;
+                if (hash == e.hash && Objects.equals(value, e.value)) {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        /**
+         * Compares this object with the specified object for order.
+         */
+        @Override
+        public int compareTo(Leaf<E> l) {
+            return hash - l.hash;
+        }
+    }
+
+
+    /**
+     * Adds value as a child of parent leaf.
+     * @param parent parent leaf
+     * @param value value
+     * @return true if value was added
+     */
     public boolean addChild(Leaf<E> parent, E value) {
         boolean result = false;
         if (parent.left != null && parent.right != null) {
