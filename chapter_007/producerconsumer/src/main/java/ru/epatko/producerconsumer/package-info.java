0fdec67bf80a38f714be20f17e8513d9e3d50106
Mikhail <mikhail.epatko@gmail.com>
2017-03-28 21:38:05 +1000

    added ProducerConsumer

---
+++ chapter_007/producerconsumer/src/main/java/ru/epatko/producerconsumer/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 1. Реализовать шаблон Producer Cunsumer на основании блокирующей очереди.
+ * Producer - добавляет данные, а Cunsomer их забирает. Если данных нет, поток блокируется до появления новых данных.
+ *
+ *
+ *
+ *
+ * @author Mikhail Epatko (mikhail.epatko@gmail.com).
+ * 28.03.17
+ */
+package ru.epatko.producerconsumer;
